0ea34a734e8439bb53001fd0370ee38da7057616
adeyinkabadmus <adeyinkabadmus@gmail.com>
2020-01-07 17:28:50 +0100

    remove messages from view and change status in db

--- lib/services/http_url_config.dart
+++ lib/services/http_url_config.dart
@@ -1,10 +1,11 @@
 const config = {
-  "baseUrl": "http://api.facilng.net/estate/web/",
-  //"baseUrl": "http://192.168.43.61/estate/web/",
+  //"baseUrl": "http://api.facilng.net/estate/web/",
+  "baseUrl": "http://192.168.43.61/estate/web/",
   "user": "user/",
   "login": "login/",
   "announcements": "announcements/",
   "message": "message/",
+  "delete": "delete/",
   "visit": "propertyaccess/",
   "create": "create/",
   "view": "view/",
